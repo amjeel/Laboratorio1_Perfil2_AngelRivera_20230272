6c1540621b9344464cb207276239421995c0f3c6
amjeel <aamjelmacarron@gmail.com>
2025-06-06 11:06:35 -0600

    crear menu

--- Laboratorio1_AngelRivera_20230272/src/main/java/AngelRivera_20230272/Main.java
+++ Laboratorio1_AngelRivera_20230272/src/main/java/AngelRivera_20230272/Main.java
@@ -1,9 +1,17 @@
 package AngelRivera_20230272;
+import java.util.TreeMap;
+import java.util.Map;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Bienvenido al sistema de gestion de productos");
+        System.out.println("Que desea hacer?");
+        System.out.println("1. Ver productos");
+        System.out.println("2. Agregar productos");
+        System.out.println("3. Editar/Actualizar productos");
+        System.out.println("4. Eliminar productos");
     }
 }
---
+++ Laboratorio1_AngelRivera_20230272/src/main/java/AngelRivera_20230272/Producto.java
@@ -0,0 +1,8 @@
+package AngelRivera_20230272;
+
+public class Producto {
+    public int id;
+    public String nombre;
+    public double precio;
+
+}
