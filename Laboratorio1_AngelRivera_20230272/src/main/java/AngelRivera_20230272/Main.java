83bf96ff42daaad2bcf8b89b4be59102b1894c39
amjeel <aamjelmacarron@gmail.com>
2025-06-06 11:16:11 -0600

    menu

--- Laboratorio1_AngelRivera_20230272/src/main/java/AngelRivera_20230272/Main.java
+++ Laboratorio1_AngelRivera_20230272/src/main/java/AngelRivera_20230272/Main.java
@@ -13,5 +13,22 @@ public class Main {
         System.out.println("2. Agregar productos");
         System.out.println("3. Editar/Actualizar productos");
         System.out.println("4. Eliminar productos");
+
+        int OpcionMenu = 4;
+
+        switch (OpcionMenu) {
+            case 1:
+            System.out.println("estos son los productos:");
+
+        }
+        TreeMap<String, Integer> productos = new TreeMap<>();
+        productos.put("Mesa", 100);
+        productos.put("Cama", 220);
+        productos.put("cocina", 400);
+        productos.put("Refri", 330);
+        productos.put("Tv", 750);
+
+        System.out.println("estos son los productos: " + productos);
     }
+
 }
